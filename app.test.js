b3a63e593d3b3247017fde0464647f71f7772454
Brandon <brandonsp0102@gmail.com>
2025-10-24 23:24:24 -0600

    first commit

---
+++ app.test.js
@@ -0,0 +1,10 @@
+// app.test.js
+const sayHello = require('./app');
+
+test('debe devolver un saludo con el nombre', () => {
+  expect(sayHello("Brandon")).toBe("Hola, Brandon!");
+});
+
+test('debe devolver Hola, Mundo! cuando el nombre es Mundo', () => {
+  expect(sayHello("Mundo")).toBe("Hola, Mundo!");
+});
